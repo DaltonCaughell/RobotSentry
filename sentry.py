c8a1cb1d083f9f680776a18e466e5fcef5e7ae4f
Devin Caplow-Munro <decamun@gmail.com>
2014-01-03 15:43:03 -0800

    sentry detects movement, speaks and sends emails. Facial Recognition on the way. This is a snapshot build.

--- sentry.py
+++ sentry.py
@@ -7,9 +7,15 @@ from speaker import say
 #import code to detect motion
 import tracker
 
+#import code for password input
+from input_timed import readInput
+import getpass
+import time
+
+USER_PASSWORD = "robats"
+
 def man_init():
     user_email = raw_input('What is your email addess? ')
-    tracker.setCam(raw_input('What camera would you like to use? (integrated is usually "0"' ))
     user_input = raw_input('Would you like to configure outgoing email as well? If you \nuse the default, email will be sent from RobotSentry@gmail.com.\n(y/n)')
     if(user_input == 'y'):
         system_hostname = raw_input('What is your hostname? ')
@@ -18,6 +24,15 @@ def man_init():
         init(user_email, system_hostname, system_login, system_password)
     else:
         init(user_email)
+    global USER_PASSWORD
+    getpassword = True
+    while getpassword:
+        password = getpass.getpass("Enter a password")
+        if(getpass.getpass("please re-enter it:") == password):
+            USER_PASSWORD = password
+            getpassword = False
+        else:
+            print "Sorry, those two didn't match"
         
 #initialize
 print '\nWelcome to Robot Sentry'
@@ -35,22 +50,36 @@ while(user_input == 'n'):
         say(txt)
         text.close()
 
+e = raw_input("Press enter to calibrate motion tracker...\n(make sure nothing is moving onscreen)")
+tracker.calibrate()
 
-print "\n tracker running..."
-
-sendAlert("blah","blah", "trollface.jpg")
+print "\nInitiation Complete:\n\ntracker running..."
 
 tickcount = 0
+movecount = 0
 
 while tracker.running():
     #Robot Sentry Main Loop
-    print 'tick#', tickcount
     tickcount += 1    
     tracker.tick()
 
     if tracker.movement():
         #Movement Detected
-        print 'detected movement... '
+        print 'detected movement... ', movecount
+        movecount += 1
+
+        if movecount > 3:
+            file1, file2, file3 = tracker.save(tickcount)
+            movecount = 0
+            seconds = 5
+            say("Intruder Alert! Intruder Alert! You have " + str(seconds) + " seconds to enter your passcode")
+            pw = readInput("Password: ", "not the password", seconds)
+            if not pw == USER_PASSWORD:
+                print "Intruder Detected! Sending alert!"
+                sendAlert("Intruder. Systime: " + str(time.time()), "Intruder", file2)
+    else:
+        if movecount > 0:
+            movecount -= 1
         
 
 #exit
--- tracker.py
+++ tracker.py
@@ -4,28 +4,112 @@ import numpy as np
 trackerRunning = True
 
 #test video capture
-cap = cv2.VideoCapture(0)
-#set the width and height
-cap.set(3,1280)
-cap.set(4,1024)
+cam = cv2.VideoCapture(0)
 
-lastret, lastimg = cap.read()
-ret, img = cap.read()
+
+_, i1 = cam.read()
+i2 = i1
+i3 = i1
+moved = False
+
+avg_diff = 1000 #diff threshold for movement
+
 
 print "Tracker initialized correctly using OpenCV version ", cv2.__version__
 
+
+
+#converts images to a binary threshold
+def threshold(img):
+    
+    img = cv2.medianBlur(img,5)
+
+    _, thr1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+    return thr1
+
+def diff(t1, t2, t3):
+    #compute diff images
+    d1 = cv2.absdiff(t1, t2)
+    d2 = cv2.absdiff(t2, t3)
+    diffImg = cv2.bitwise_and(d1, d2)
+    diffImg = cv2.cvtColor(diffImg, cv2.COLOR_RGB2GRAY)
+    return diffImg
+
 def tick():
-    
-    ret, img = cap.read()
-    
-    cv2.imshow("input", img)
+
+    #pull globals
+    global i1
+    global i2
+    global i3
+    global avg_diff
+
+    #get images
+    i1 = i2
+    i2 = i3
+    _, i3 = cam.read()
+
+    #threshold images
+    t1 = threshold(i1)
+    t2 = threshold(i2)
+    t3 = threshold(i3)
+
+    diffImg = diff(t1, t2, t3)
+
+    #show images
+    cv2.imshow("diff", diffImg)
+    cv2.imshow("original", i2)
+
+    #compute number of changed pixels and adjust weighted average
+    count = cv2.countNonZero(diffImg)
+    avg_diff = (avg_diff * 0.95) + (count * 0.05)
+
+    #check for movement
+    global moved
+    moved = count > avg_diff * 1.3
+
+
+    #wait 50
+    k = cv2.waitKey(50) & 0xFF
+    if k == 27:
+        #user hit escape
+        cv2.destroyAllWindows()
+        print "Escape Key"
+        global trackerRunning
+        trackerRunning = False
+
+
+
 
 def running():
     return trackerRunning
 
 def movement():
-    return False
+    return moved
 
-def setCam(cam):
-    global cap
-    cap = cv2.VideoCapture(cam)
+
+def calibrate():
+    tick()
+    tick()
+    tick()
+    count = cv2.countNonZero(diff(threshold(i1), threshold(i2), threshold(i3)))
+    global avg_diff
+    avg_diff = count
+    print "Tracker Calibrated, diff: ", count
+
+def save(num):
+    file1 = "i1:" + str(num) + ".jpg"
+    file2 = "i2:" + str(num) + ".jpg"
+    file3 = "i3:" + str(num) + ".jpg"
+    cv2.imwrite(file1, i1)
+    cv2.imwrite(file2, i1)
+    cv2.imwrite(file3, i1)
+    return file1, file2, file3
+
+def setCam(camNum):
+    global cam
+    cam = cv2.VideoCapture(camNum)
+
+def wait(t):
+    k = cv2.waitKey(t) & 0xFF
+    return k
